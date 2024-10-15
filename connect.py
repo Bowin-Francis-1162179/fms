1d1d39b7e7f740eb83c3cdc43bc4d4175013c01c
Bowin-Francis-1162179 <Bowin.Francis@lincolnuni.ac.nz>
2024-10-15 23:01:05 +1300

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,8 @@
+# This is the local version: These details are available on the first MySQL Workbench screen - usually called 'Local Instance'
+# PythonAnywhere version: Copy details from Database page and change values.
+# Add this file to .gitignore so it is not synced to GitHub.
+dbuser = "BowinFrancis1162" #PUT YOUR MySQL username here
+dbpass = "Optimize@1989" #PUT YOUR PASSWORD HERE - make sure this matches db password set in PA..
+dbhost = "BowinFrancis1162179.mysql.pythonanywhere-services.com" 
+dbport = "3306"     # Same in local and PA version
+dbname = "fms"
